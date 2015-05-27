d46eb71aa85212510dc2a9af5cb66ec7f3f5fa4f
GeorgeKA <k._asante@hotmail.com>
2015-05-27 15:38:49 -0400

    "Added CSS3 animation support for Webkit browsers."

--- js/shortStory.js
+++ js/shortStory.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
-	 setTimeout(function(){         
+    // Move away loading screen
+    setTimeout(function(){
          document.getElementById('loadingDiv').classList.add("loaded");
     }, 3000);
     
+    // Add effects to the loading screen
     setInterval(function(){
         document.getElementById("loadingEllipses").classList.toggle("loadingVisibilityToggle");
         document.getElementById("loadingString").classList.add("loadingColourToggle");        
